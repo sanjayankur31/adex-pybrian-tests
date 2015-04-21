046155054d50ba2f0915eb697fd61db6ba95f857
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2015-04-21 12:01:01 +0100

    4g - Completely different from paper
    The paper shows one solitary spike. This shows many many spikes,
    possibly irregular firing.

--- adex.py
+++ adex.py
@@ -22,18 +22,16 @@
 from brian2 import *
 
 # Parameters
-C = 281 * pF
-gL = 30 * nS
+C = 100 * pF
+gL = 10 * nS
 taum = C / gL
-EL = -70.6 * mV
-VT = -50.4 * mV
+EL = -65 * mV
+VT = -50 * mV
 DeltaT = 2 * mV
-Vcut = VT + 5 * DeltaT
+Vcut = 0 * mV
 
 # Pick an electrophysiological behaviour
-tauw, a, b, Vr = 144*ms, 4*nS, 0.0805*nA, -70.6*mV # Regular spiking (as in the paper)
-#tauw,a,b,Vr=20*ms,4*nS,0.5*nA,VT+5*mV # Bursting
-#tauw,a,b,Vr=144*ms,2*C/(144*ms),0*nA,-70.6*mV # Fast spiking
+tauw, a, b, Vr = 90*ms, -10*nS, 0.03*nA, -47*mV # 4g
 
 eqs = """
 dvm/dt = (gL*(EL - vm) + gL*DeltaT*exp((vm - VT)/DeltaT) + I - w)/C : volt
@@ -47,11 +45,8 @@ neuron.vm = EL
 trace = StateMonitor(neuron, 'vm', record=0)
 spikes = SpikeMonitor(neuron)
 
-run(20 * ms)
-neuron.I = 1*nA
-run(100 * ms)
-neuron.I = 0*nA
-run(20 * ms)
+neuron.I = .350*nA
+run(1000 * ms)
 
 # We draw nicer spikes
 vm = trace[0].vm[:]
