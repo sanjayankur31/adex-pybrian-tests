a24c469c76cc4e0a069c821e1791862cf3494f10
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2015-04-22 11:06:59 +0100

    Added readme.

---
+++ README.rst
@@ -0,0 +1,8 @@
+AdEx-pybrian-tests
+------------------
+
+This repository just contains tests that I run on the AdEx neuron model `(Brett and Gerstner (2005))`_ to check the parameters given in `Naud et al. (2008)`_ using Brian2_
+
+.. _(Brett and Gerstner (2005)): http://jn.physiology.org/content/94/5/3637.short
+.. _Naud et al. (2008): http://link.springer.com/article/10.1007/s00422-008-0264-7
+.. _Brian2: https://pypi.python.org/pypi/Brian2
--- adex.py
+++ adex.py
@@ -22,16 +22,16 @@
 from brian2 import *
 
 # Parameters
-C = 200 * pF
-gL = 12 * nS
+C = 100 * pF
+gL = 10 * nS
 taum = C / gL
-EL = -70 * mV
+EL = -65 * mV
 VT = -50 * mV
 DeltaT = 2 * mV
 Vcut = 0 * mV
 
 # Pick an electrophysiological behaviour
-tauw, a, b, Vr = 300*ms, -6*nS, 0*nA, -58*mV
+tauw, a, b, Vr = 90*ms, -10*nS, 0.03*nA, -47*mV # 4g
 
 eqs = """
 dvm/dt = (gL*(EL - vm) + gL*DeltaT*exp((vm - VT)/DeltaT) + I - w)/C : volt
@@ -45,10 +45,8 @@ neuron.vm = EL
 trace = StateMonitor(neuron, 'vm', record=0)
 spikes = SpikeMonitor(neuron)
 
-neuron.I = 0*nA
-run(100 * ms)
-neuron.I = .110*nA
-run(5000 * ms)
+neuron.I = .350*nA
+run(1000 * ms)
 
 # We draw nicer spikes
 vm = trace[0].vm[:]
