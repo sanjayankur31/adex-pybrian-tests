6205e1882228497c0a90826b7332ce4ccaf62d21
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2015-04-22 10:24:03 +0100

    4d looks good.

--- adex.py
+++ adex.py
@@ -22,8 +22,8 @@
 from brian2 import *
 
 # Parameters
-C = 130 * pF
-gL = 18 * nS
+C = 200 * pF
+gL = 10 * nS
 taum = C / gL
 EL = -58 * mV
 VT = -50 * mV
@@ -31,7 +31,7 @@ DeltaT = 2 * mV
 Vcut = 0 * mV
 
 # Pick an electrophysiological behaviour
-tauw, a, b, Vr = 150*ms, 4*nS, .120*nA, -50*mV # 4b
+tauw, a, b, Vr = 120*ms, 2*nS, .100*nA, -46*mV
 
 eqs = """
 dvm/dt = (gL*(EL - vm) + gL*DeltaT*exp((vm - VT)/DeltaT) + I - w)/C : volt
@@ -47,7 +47,7 @@ spikes = SpikeMonitor(neuron)
 
 neuron.I = 0*nA
 run(100 * ms)
-neuron.I = .400*nA
+neuron.I = .210*nA
 run(1000 * ms)
 
 # We draw nicer spikes
