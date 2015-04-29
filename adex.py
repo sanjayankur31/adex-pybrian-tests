a6036448680eed0cd143cf72409e945ca9e0da68
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2015-04-29 17:01:33 +0100

    4d is now perfect.

--- adex.py
+++ adex.py
@@ -22,16 +22,16 @@
 from brian2 import *
 
 # Parameters
-C = 100 * pF
-gL = 12 * nS
+C = 200 * pF
+gL = 10 * nS
 taum = C / gL
-EL = -60 * mV
+EL = -58 * mV
 VT = -50 * mV
 DeltaT = 2 * mV
 Vcut = 0 * mV
 
 # Pick an electrophysiological behaviour
-tauw, a, b, Vr = 130*ms, -11*nS, 0.03*nA, -48*mV # 4g
+tauw, a, b, Vr = 120*ms, 2*nS, .100*nA, -46*mV
 
 eqs = """
 dvm/dt = (gL*(EL - vm) + gL*DeltaT*exp((vm - VT)/DeltaT) + I - w)/C : volt
@@ -45,7 +45,7 @@ neuron.vm = EL
 trace = StateMonitor(neuron, 'vm', record=0)
 spikes = SpikeMonitor(neuron)
 
-neuron.I = .160*nA
+neuron.I = .400*nA
 run(1000 * ms)
 
 # We draw nicer spikes
